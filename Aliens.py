1c81eb6f10284d61f15a7e68a051909c80c491d2
Jonathan Sayce <jsayce@sedgely.com>
2024-09-27 19:08:00 +0100

    Added a third load of squares

--- Aliens.py
+++ Aliens.py
@@ -2,15 +2,22 @@ from tkinter import *
 window = Tk()
 canvas = Canvas(window, width=2000, height=1500)
 canvas.pack()
-squares = 100
+squares = 50
 for squarenumber in range(squares):
-    blue_value = max(0, 255 - squarenumber * 4)
-    green_value = min(255, squarenumber * 4)
+    blue_value = max(0, 255 - squarenumber * 5)
+    green_value = min(255, squarenumber * 5)
     color = f'#{0:02x}{green_value:02x}{blue_value:02x}'
     canvas.create_rectangle(10 + squarenumber * 10, 10 + squarenumber * 10, 200 + squarenumber * 10, 200 + squarenumber * 10, fill=color)
-# for squarenumber in range(squares):
-#     red_value = max(0, 255 - squarenumber * 4)
-#     green_value = min(255, squarenumber * 4)
-#     color = f'#{0:02x}{green_value:02x}{blue_value:02x}'
-#     canvas.create_rectangle(10 + squarenumber * 10, 10 + squarenumber * 10, 200 + squarenumber * 10, 200 + squarenumber * 10, fill=color)
+for squarenumber in range(squares):
+    red_value = min(0, 255 - squarenumber * 5)
+    green_value = max(255, squarenumber * 5)
+    blue_value = max(0, 255 - squarenumber * 5)
+    color = f'#{red_value:02x}{green_value:02x}{0:02x}'
+    canvas.create_rectangle(510 + squarenumber * 10, 10 + squarenumber * 10, 700 + squarenumber * 10, 200 + squarenumber * 10, fill=color)
+for squarenumber in range(squares):
+    red_value = max(0, 255 - squarenumber * 5)
+    green_value = max(0, 255 - squarenumber * 5)
+    blue_value = min(255, squarenumber * 5)
+    color = f'#{red_value:02x}{green_value:02x}{blue_value:02x}'
+    canvas.create_rectangle(10 + squarenumber * 10, 510 - squarenumber * 10, 200 + squarenumber * 10, 700 - squarenumber * 10, fill=color)
 window.mainloop()
